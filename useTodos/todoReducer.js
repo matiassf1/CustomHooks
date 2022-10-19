088e9f9dd4edc6300e441cb4f92bae6c01b726ce
Matias Sfer <matias0developer@gmail.com>
2022-10-19 11:22:24 -0300

    useTodos agregados

---
+++ useTodos/todoReducer.js
@@ -0,0 +1,23 @@
+export const todoReducer = (initialState = [], action) => {
+
+    switch ( action.type ) {
+        case '[TODO] Add Todo':
+            return [ ...initialState, action.payload ]
+    
+        case '[TODO] Delete Todo':
+            return initialState.filter( todo => todo.id != action.payload )
+        
+        case '[TODO] Modify Todo':
+            return initialState.map((todo) => {
+                if (todo.id === action.payload) {
+                       todo = {
+                        ...todo,
+                        done: !(todo.done)
+                    }
+                }
+                return todo
+            })
+        default:
+            return initialState;
+    }
+}
